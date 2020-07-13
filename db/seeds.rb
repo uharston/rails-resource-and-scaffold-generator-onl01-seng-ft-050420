e6c1231f20fe6552324a4dd658e63944b05db92b
uharston <uharston@gmail.com>
2020-07-12 19:13:06 -0500

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Article.create(title: "MY STORY", body: "It started a long time ago")
+Article.create(title: "Your life", body: "Let's run away")
+Article.create(title: "Our Family", body: "Let's stay together")
